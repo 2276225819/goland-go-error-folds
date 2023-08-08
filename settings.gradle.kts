04cd0ca2c60e0d17fbe4dad858385a16db53e7d3
xlx <2276225819@qq.com>
2023-08-08 19:37:49 +0800

    support if !ok

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "demoplugin"
+rootProject.name = "gofolderror"
--- src/main/java/org/example/gofolderror/GoBlockFoldingBuilder.java
+++ src/main/java/org/example/gofolderror/GoBlockFoldingBuilder.java
@@ -18,18 +18,20 @@ public class GoBlockFoldingBuilder implements FoldingBuilder {
     @Override
     public FoldingDescriptor [] buildFoldRegions(@NotNull ASTNode root, @NotNull Document document) {
 
-        Pattern pattern = Pattern.compile( "(\\S+)\\s+:?=([^\\r\\n]+)[\\r\\n]+\\s*if (\\S+) != nil [{][\r\n]\\s+([^\r\n]*[\r\n]+)\\s+[}](?=[\r\n])");
-        Matcher matcher = pattern.matcher(document.getText());
+        String txt = document.getText();
 
+        Pattern pattern = Pattern.compile( "(\\S+)\\s+:?=([^\\r\\n]+)[\\r\\n]+\\s*(?:if (?<t1>\\S+) != nil |if !(?<t2>\\S+) )[{][\r\n]\\s+(?<v>[^\r\n]*[\r\n]+)\\s+[}](?=[\r\n])");
+        Matcher matcher = pattern.matcher(txt);
         List<FoldingDescriptor> lx = new ArrayList<>();
         while (matcher.find()) {
-            String a=matcher.group(1);
-            String b=matcher.group(3);
-            if(a.equals(b)){
-                lx.add(new FoldingDescriptor(root,
-                        new TextRange(   matcher.end(2),matcher.end(0)),
-                        null, " ! "+ matcher.group(4) + " ")
-                );
+            String a = matcher.group(1);
+            String t1= matcher.group("t1");
+            String t2= matcher.group("t2");
+            String r = matcher.group("v");
+            int ss =  matcher.end(2);
+            int ee = matcher.end(0);
+            if(a.equals(t1) || a.equals(t2)){
+                lx.add(new FoldingDescriptor(root,new TextRange( ss,ee), null, " ! "+ r + " "));
             }
         }
 
